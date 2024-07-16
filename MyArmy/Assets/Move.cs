bd022764600ecfbfc62708d795a9522efa82d255
OleksandrShulha <106068375+OleksandrShulha@users.noreply.github.com>
2024-07-16 11:55:14 +0300

    Work v.4

--- MyArmy/Assets/Move.cs
+++ MyArmy/Assets/Move.cs
@@ -11,27 +11,25 @@ public class Move : MonoBehaviour
     [SerializeField] private float speed = 2f;
     [SerializeField] private float hp = 200f;
 
+
     public bool go = true;
     public bool isHaveTarget = false;
     public bool isFindTarget = false;
 
-    private Vector3 targetPointToMove;
+    private Vector3 targetToMoveBase;
+    private Transform targetToMoveEnemy;
+    private GameObject enemyBase;
 
-    private Transform targetToMove;
-    private Transform targetToBaseMove;
 
     private Animator anim;
 
 
-
-
     void Start()
     {
-
         anim = GetComponent<Animator>();
 
-        GameObject enemyBase = GameObject.FindGameObjectWithTag("EnemyBase");
-        targetPointToMove = new Vector3(enemyBase.transform.position.x, (Random.Range(-2.5f, 0.8f)), 0f);
+        enemyBase = GameObject.FindGameObjectWithTag("EnemyBase");
+        targetToMoveBase = new Vector3(enemyBase.transform.position.x, (Random.Range(-2.5f, 0.8f)), 0f);
     }
 
     void Update()
@@ -46,39 +44,61 @@ public class Move : MonoBehaviour
     {
         if (isFindTarget)
         {
-            float dist = Vector3.Distance(targetToMove.position, transform.position);
+            float dist = Vector3.Distance(targetToMoveEnemy.position, transform.position);
             if (dist < 0.5)
             {
                 isHaveTarget = true;
-                go = false;
+            }
+            else
+            {
+                isHaveTarget = false;
+            }
+
+        }
+        if(!isFindTarget && enemyBase != null)
+        {
+            float dist = Vector3.Distance(targetToMoveBase, transform.position);
+            if (dist < 1.5f)
+            {
+                isHaveTarget = true;
             }
             else
             {
                 isHaveTarget = false;
             }
         }
+
+
+        if(!isFindTarget && enemyBase == null)
+        {
+            isHaveTarget = false;
+        }
     }
 
     public void AnimationHero()
     {
-        if (!go || !isHaveTarget && !isFindTarget && hp>0)
+        if (hp <= 0)
         {
+            //Dead
+            anim.SetInteger("State", 4);
+        }
+        if (!go && hp >0)
+        {
+            //Idle
             anim.speed = 1F;
             anim.SetInteger("State", 1);
         }
-        if (go || isFindTarget && !isHaveTarget && hp > 0)
+        if(go && !isHaveTarget && hp >0)
         {
+            //Move
             anim.speed = 1F;
             anim.SetInteger("State", 2);
         }
-        if (!go && isHaveTarget && isFindTarget && hp > 0)
+        if (go && isHaveTarget && hp > 0)
         {
+            //Atack
             anim.speed = 1F;
             anim.SetInteger("State", 3);
-        }
-        if (hp <= 0)
-        {
-            anim.SetInteger("State", 4);
         }
     }
 
@@ -87,14 +107,16 @@ public class Move : MonoBehaviour
         List<GameObject> enemyes = new List<GameObject>(GameObject.FindGameObjectsWithTag("Enemies"));
         if (enemyes.Count > 0)
         {
+
+            go = true;
             float curentDist = Vector3.Distance(enemyes[0].transform.position, transform.position);
-            targetToMove = enemyes[0].transform;
+            targetToMoveEnemy = enemyes[0].transform;
             foreach (GameObject enemy in enemyes)
             {
                 float dist = Vector3.Distance(enemy.transform.position, transform.position);
                 if (dist < curentDist)
                 {
-                    targetToMove = enemy.transform;
+                    targetToMoveEnemy = enemy.transform;
                     curentDist = dist;
                 }
             }
@@ -102,8 +124,16 @@ public class Move : MonoBehaviour
         }
         else
         {
+            enemyBase = GameObject.FindGameObjectWithTag("EnemyBase");
             isFindTarget = false;
-            isHaveTarget = false;
+            if(enemyBase != null)
+            {
+                go = true;
+            }
+            else
+            {
+                go = false;
+            }
         }
 
     }
@@ -111,31 +141,31 @@ public class Move : MonoBehaviour
 
     public void MoveToTarget()
     {
-        if (!isFindTarget && !isHaveTarget)
+        if (!isFindTarget && !isHaveTarget && enemyBase != null && hp>0)
         {
-            transform.position = Vector3.MoveTowards(transform.position, targetPointToMove, Time.deltaTime * speed);
+            transform.position = Vector3.MoveTowards(transform.position, targetToMoveBase, Time.deltaTime * speed);
+            if(transform.position.x > targetToMoveBase.x)
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+            else
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
         }
 
-        if (isFindTarget && !isHaveTarget)
+        if (isFindTarget && !isHaveTarget && hp > 0)
         {
-            transform.position = Vector3.MoveTowards(transform.position, targetToMove.position, Time.deltaTime * speed);
+            transform.position = Vector3.MoveTowards(transform.position, targetToMoveEnemy.position, Time.deltaTime * speed);
+            if (transform.position.x > targetToMoveEnemy.position.x)
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+            else
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
         }
-
-
-
-
-
-        //if (go)
-        //{
-        //    transform.position = Vector3.MoveTowards(transform.position, targetToMove.position, Time.deltaTime * speed);
-        //}
-
-        //if(!go && !isFindTarget)
-        //{
-        //    GameObject enemyBase = GameObject.FindGameObjectWithTag("EnemyBase");
-        //    transform.position = Vector3.MoveTowards(transform.position, enemyBase.transform.position, Time.deltaTime * speed);
-        //}
-        
     }
 }
 
